02dd7217248472783a84758709e8f324717cd87c
Alvaro Andrade <siruz_slipknot@hotmail.com>
2019-03-14 15:13:57 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require 'functions/init.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
  
-        <title>Login - Mr Slime</title>
+        <title>Autenticação - Mr Slime</title>
 		
 	<!-- Bootstr-ap -->
     <link href="_css/bootstrap.min.css" rel="stylesheet">
