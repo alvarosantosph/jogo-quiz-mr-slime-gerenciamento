49fa84124b927dd2246edb017686e488ac88f8f5
Alvaro Santos <siruz_slipknot@hotmail.com>
2019-03-23 07:47:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -26,16 +26,16 @@ require 'functions/init.php';
     </head>
  
     <body>
-         
-		<center><img src="_imagens/mr-slime-logo-alterado.png" heignt="600" width="600" alt="SLIME" title"SLIME"></center>
-		
-	            <h1>Mr Slime - Gerenciamento de Perguntas e Respostas </h1>
- 
-        <?php if (isLoggedIn()): ?>
+	    
+	            <?php if (isLoggedIn()): ?>
             <p>Olá, <?php echo $_SESSION['user_name']; ?>. <a href="cadastro/cadastro.php">Painel</a> | <a href="login/logout.php">Sair</a></p>
         <?php else: ?>
             <p>Olá, visitante. <a href="login/form-login.php">Login</a></p>
         <?php endif; ?>
+         
+		<center><img src="_imagens/mr-slime-logo-alterado.png" heignt="600" width="600" alt="SLIME" title"SLIME"></center>
+		
+	    <h1>Mr Slime - Gerenciamento de Perguntas e Respostas </h1>
  
     </body>
 </html>
