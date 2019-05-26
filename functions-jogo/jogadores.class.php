945056ac76a90e2dc411936a9a325c1a64c8baaf
Alvaro Santos <siruz_slipknot@hotmail.com>
2019-05-26 18:50:34 -0300

    Add files via upload

---
+++ functions-jogo/jogadores.class.php
@@ -0,0 +1,29 @@
+<?php 
+require_once('game.class.php');
+
+class Jogadores extends Game {
+	
+	public function __construct(){
+		parent::__construct('player');
+	}
+	
+	public function verificaJogador($email,$password) {
+		return $this->query("SELECT * FROM $this->tabela WHERE email ='$email' AND password ='$password'")->Fetch();
+		
+	}
+	
+	public function verificaEmail($email) {
+		return $this->query("SELECT * FROM $this->tabela WHERE email ='$email'")->Fetch();
+		
+	}
+	
+	public function buscaPerguntas() {
+		return $this->query("SELECT * FROM selecionar_perguntas")->FetchAll();
+		
+	}
+	
+	public function buscaJogadores() {
+		return $this->query("SELECT * FROM player ORDER BY pontuacao DESC LIMIT 10")->FetchAll();
+		
+	}
+}
