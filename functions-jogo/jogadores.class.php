0fb0eebd9683a83b649a403a5d0a0b611d60b36e
Alvaro Santos <siruz_slipknot@hotmail.com>
2019-05-29 12:49:34 -0300

    Update jogadores.class.php

--- functions-jogo/jogadores.class.php
+++ functions-jogo/jogadores.class.php
@@ -19,7 +19,7 @@ class Jogadores extends Game {
 	
 	
 	public function verificaEmailAtualizar($email) {
-		return $this->query("SELECT * FROM $this->tabela WHERE email ='$email' AND email !='$email'")->Fetch();
+		return $this->query("SELECT DISTINCT * FROM $this->tabela WHERE email ='$email'")->Fetch();
 		
 	}
 	
